7416d193b981ca804303b2f194716372801288e7
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-06-03 12:39:20 +0800

    Add docker file and fix hostname.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const express = require('express');
-const { ppid } = require('process');
 
 const app = express();
 const port = process.env.PORT || 80;
-const hostname = process.env.HOSTNAME || '127.0.0.1';
+const hostname = process.env.HOSTNAME || '0.0.0.0';
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
@@ -14,4 +13,4 @@ app.get('/hello', (req, res) => {
     res.json({'hello': 'world'});
 });
 
-app.listen(port, () => console.log(`Server started at http://${hostname}:${port}/`));
+app.listen(port, hostname, () => console.log(`Server started at http://${hostname}:${port}/`));
