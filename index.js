dfb61d25f1a7cf5961f923e0f229888a15791ba3
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-06-03 12:48:32 +0800

    Dont use env var for port and hostname.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 80;
-const hostname = process.env.HOSTNAME || '0.0.0.0';
+const port = 80;
+const hostname = '0.0.0.0';
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
